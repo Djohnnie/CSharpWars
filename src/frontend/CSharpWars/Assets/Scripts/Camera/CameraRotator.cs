6b58d99b190eed3d2c34c251bacc93ecc8773c85
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2019-02-22 19:04:31 +0100

    Initial import of front-end ideas in Unity3D

---
+++ src/frontend/CSharpWars/Assets/Scripts/Camera/CameraRotator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    #region <| Public Properties |>
+
+    public Single RotationsPerMinute = 10.0f;
+
+    #endregion
+
+    void Update()
+    {
+        transform.Rotate(0f, 6.0f * RotationsPerMinute * Time.deltaTime, 0f);
+    }
+}
