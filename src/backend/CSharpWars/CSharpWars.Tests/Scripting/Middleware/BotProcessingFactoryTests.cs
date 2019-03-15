0dc078f1158b329c219005fff885e31537adc977
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2019-03-15 20:25:37 +0100

    Added postprocessor unittests

--- src/backend/CSharpWars/CSharpWars.Tests/Scripting/Middleware/BotProcessingFactoryTests.cs
+++ src/backend/CSharpWars/CSharpWars.Tests/Scripting/Middleware/BotProcessingFactoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using CSharpWars.Common.Extensions;
 using CSharpWars.DtoModel;
 using CSharpWars.Enums;
 using CSharpWars.Logging.Interfaces;
@@ -38,6 +39,33 @@ namespace CSharpWars.Tests.Scripting.Middleware
 
             // Assert
             context.GetBotProperties(bot.Id).CurrentMove.Should().Be(PossibleMoves.ScriptError);
+        }
+
+        [Fact]
+        public async Task BotProcessingFactory_Process_With_Valid_Script_Should_Register_Correct_Move()
+        {
+            // Arrange
+            var botLogic = new Mock<IBotLogic>();
+            var botScriptCompiler = new BotScriptCompiler();
+            var botScriptCache = new BotScriptCache();
+            var logger = new Mock<ILogger>();
+            var botProcessingFactory = new BotProcessingFactory(
+                botLogic.Object, botScriptCompiler, botScriptCache, logger.Object);
+            var arena = new ArenaDto { Width = 4, Height = 3 };
+            var bot = new BotDto { Id = Guid.NewGuid() };
+            var bots = new List<BotDto>(new[] { bot });
+            var context = ProcessingContext.Build(arena, bots);
+            context.AddBotProperties(bot.Id, BotProperties.Build(bot, arena, bots));
+            var botScript = "WalkForward();".Base64Encode();
+
+            // Mock
+            botLogic.Setup(x => x.GetBotScript(It.IsAny<Guid>())).ReturnsAsync(botScript);
+
+            // Act
+            await botProcessingFactory.Process(bot, context);
+
+            // Assert
+            context.GetBotProperties(bot.Id).CurrentMove.Should().Be(PossibleMoves.WalkForward);
         }
     }
 }
---
+++ src/backend/CSharpWars/CSharpWars.Tests/Scripting/Middleware/PostprocessorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpWars.Common.Helpers.Interfaces;
+using CSharpWars.DtoModel;
+using CSharpWars.Enums;
+using CSharpWars.Scripting.Model;
+using CSharpWars.ScriptProcessor.Middleware;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CSharpWars.Tests.Scripting.Middleware
+{
+    public class PostprocessorTests
+    {
+        [Fact]
+        public async Task Postprocessor_Go_Should_Work()
+        {
+            // Arrange
+            var randomHelper = new Mock<IRandomHelper>();
+            var postprocessor = new Postprocessor(randomHelper.Object);
+            var arena = new ArenaDto { Width = 1, Height = 3 };
+            var bot = new BotDto { Id = Guid.NewGuid(), X = 0, Y = 2, Orientation = PossibleOrientations.North, CurrentStamina = 10 };
+            var bots = new List<BotDto>(new[] { bot });
+            var context = ProcessingContext.Build(arena, bots);
+            var botProperties = BotProperties.Build(bot, arena, bots);
+            botProperties.CurrentMove = PossibleMoves.WalkForward;
+            context.AddBotProperties(bot.Id, botProperties);
+
+            // Act
+            await postprocessor.Go(context);
+
+            // Assert
+            context.Bots.Should().HaveCount(1);
+            context.Bots.Single().X.Should().Be(0);
+            context.Bots.Single().Y.Should().Be(1);
+            context.Bots.Single().CurrentStamina.Should().Be(9);
+        }
+    }
+}
