b07ce1875ac0d4ae9922d990a60604297e5b78e5
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2019-08-05 15:40:24 +0200

    Updated frontent to Unity 2019.2.0f1

--- src/frontend/CSharpWars/Assets/Scripts/Networking/ApiClient.cs
+++ src/frontend/CSharpWars/Assets/Scripts/Networking/ApiClient.cs
@@ -7,8 +7,7 @@ namespace Assets.Scripts.Networking
 {
     public static class ApiClient
     {
-        private static readonly String _baseUrl = "http://my.djohnnie.be:8992/api";
-        //private static readonly String _baseUrl = "http://localhost:5000/api";
+        private static readonly String _baseUrl = "http://csharpwars.azurefd.net/api";
 
         public static Arena GetArena()
         {
